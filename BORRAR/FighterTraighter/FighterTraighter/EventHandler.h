03d57b2948e00fa738408806afb75e1a8fc92640
Amph <amprubio@ucm.es>
2020-02-13 18:16:32 +0100

    añadidas clases en base a la arquitectura acordada

--- BORRAR/FighterTraighter/FighterTraighter/EventHandler.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <SDL.h>
-
-class EventHandler
-{
-public:
-	//method which character will inherance for handling their events
-	virtual void HandleEvents(SDL_Event& ev)=0; 
-};
---
+++ MF-FighterTraighter/MF-FighterTraighter/MainMenu.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "GameState.h"
+class MainMenu: public GameState
+{
+};
+
