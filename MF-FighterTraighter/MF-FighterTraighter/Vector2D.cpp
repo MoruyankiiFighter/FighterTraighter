8e0c8dfea49656520397820caaa741ce6a732efe
Eloy Moreno <eloymore@ucm.es>
2020-02-23 18:55:30 +0100

    Ampliado Vector2D para tener métodos set.

--- MF-FighterTraighter/MF-FighterTraighter/Vector2D.cpp
+++ MF-FighterTraighter/MF-FighterTraighter/Vector2D.cpp
@@ -5,6 +5,11 @@ Vector2D::Vector2D(): _x(0),_y(0)	{}
 //constructor with parameters
 Vector2D::Vector2D(double x, double y) : _x(x), _y(y)	{}
 
+Vector2D::Vector2D(const Vector2D& v)
+{
+	set(v);
+}
+
 //operator +
 Vector2D Vector2D::operator+(const Vector2D& v) const
 {
@@ -32,4 +37,10 @@ double Vector2D::operator*(const Vector2D& v)
 	w._x = this->_x * v._x;
 	w._y = this->_y * v._y;
 	return w._x+w._y;
+}
+
+Vector2D Vector2D::operator=(const Vector2D& v)
+{
+	set(v);
+	return *this;
 }
