5d2462e2f05b2696afda7b39d19ca39a8e9d01b0
Eloy Moreno <eloymore@ucm.es>
2020-02-24 13:00:04 +0100

    Puesta flag para comprobar si hay memory leaks.

--- src/MF-FighterTraighter/MF-FighterTraighter/main.cpp
+++ src/MF-FighterTraighter/MF-FighterTraighter/main.cpp
@@ -5,6 +5,7 @@
 #include "App.h"
 
 int main(int argi, char* argc[]) {
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF); // check for memory leaks
 	App app = App();
 	app.run();
 	return 0;
