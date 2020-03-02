eb2d43245be656ca2b60af1e3e3f2d04808e95b2
PabloCubells <pablocub@ucm.es>
2020-03-02 18:23:40 +0100

    Collider init y render
    La inicialización debería estar en el init.
    (Solucionado el error de que "ya tiene cuerpo")

--- src/MF-FighterTraighter/MF-FighterTraighter/Collider.cpp
+++ src/MF-FighterTraighter/MF-FighterTraighter/Collider.cpp
@@ -1,13 +1,20 @@
 #include "Collider.h"
 
-Collider::Collider(Entity* e)
-{
+Collider::Collider(Entity* e) :
+	Component(ecs::Collider), 
+	en_(e)	
+{	
+}
 
+
+void Collider::init()
+{
+	tr_ = en_->getComponent<Transform>(ecs::Transform);
 }
 
 void Collider::update()
 {
-	pos_ = entity_->getComponent<Transform>(ecs::Transform)->getPosition();
+	pos_ = tr_->getPosition();
 	/*enum posicion{stand,onAir,crouch};
 	switch (posicion)
 	{
@@ -31,4 +38,9 @@ void Collider::update()
 void Collider::render()
 {
 	//pintar linea visual
+}
+
+Collider::~Collider()
+{
+
 }
--- src/MF-FighterTraighter/MF-FighterTraighter/Collider.h
+++ src/MF-FighterTraighter/MF-FighterTraighter/Collider.h
@@ -3,20 +3,20 @@
 #include "Transform.h"
 #include "RenderImage.h"
 
-class Collider :
-	public Component
+class Collider : public Component
 {
 public:
 	Collider(Entity* e);
-	virtual void init() {} // get required pointers at runtime
-	//virtual void handleInput() {} // gets called before update, use for input
-	virtual void update() {} // main update of the component
-	virtual void render() {
-		
-	} // render textures
+	~Collider();
+	virtual void update(); // main update of the component
+	virtual void init(); // get required pointers at runtime
+	//virtual void handleInput(); // gets called before update, use for input
+	virtual void render(); // render textures
 private:
 	//Transform* tr_;
 	Vector2D pos_;
+	Entity* en_;
+	Transform* tr_;
 	std::string filePath = "../../../../assets/Assets/hitbox.png";
 
 };
