f86835e2b2abace9fbe482cab181a03e90a1587b
Pablo <prodra02@ucm.es>
2020-03-02 18:52:26 +0100

    añadido physics transform pero no funciona

--- src/MF-FighterTraighter/MF-FighterTraighter/Fight.h
+++ src/MF-FighterTraighter/MF-FighterTraighter/Fight.h
@@ -11,12 +11,9 @@ public:
 	virtual void render() override;
 	virtual ~Fight();
 private:
-	b2Body* body_;
-	b2Body* body2_;
+
 
 	b2World* world;
-	//void drawSquare(b2Vec2 points [4], b2Vec2 GetWorldCenter(), float32 GetAngle());
 
-	b2Body* addRect(int x, int y, int w, int h, bool dyn=true);
 };
 
