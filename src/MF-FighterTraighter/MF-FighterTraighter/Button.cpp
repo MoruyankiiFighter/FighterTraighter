9f1f19a8f933e48b229393ed7ddba183244d5218
Amph <amprubio@ucm.es>
2020-03-03 18:28:17 +0100

    arreglado casi del todo

--- src/MF-FighterTraighter/MF-FighterTraighter/Button.cpp
+++ src/MF-FighterTraighter/MF-FighterTraighter/Button.cpp
@@ -2,11 +2,22 @@
 #include "Entity.h"
 #include "Transform.h"
 
+void Button::init()
+{
+	render_ = entity_->getComponent<RenderImage>(ecs::RenderImage);
+}
+
 void Button::handleInput()
 {
 	if (app_->getInputManager()->isMouseButtonPressed(app_->getInputManager()->Left)) {
 		cout << "click";
-		app_->PlayArcade();
+		SDL_Point p = { app_->getInputManager()->getMousePosX(),app_->getInputManager()->getMousePosY() };
+
+		if (SDL_PointInRect(&p, &render_->getDestRect())) {
+			cout << "arcade";
+		}
+
+		//app_->PlayArcade();
 	}
 
 }
--- src/MF-FighterTraighter/MF-FighterTraighter/Button.h
+++ src/MF-FighterTraighter/MF-FighterTraighter/Button.h
@@ -1,11 +1,19 @@
 #include "Component.h"
+#include "RenderImage.h"
 using callBackOnClick = void(string text);
 
 class Button : public Component {
 
 public:
+	//constructor
 	Button() : Component(ecs::Button) {};
-	~Button() {};
+	//destructor
+	virtual ~Button() {};
+
+	void init() override;
+	//handle the input of the mouse by the moment
 	void handleInput() override;
+
 private:
+	RenderImage* render_;
 };
