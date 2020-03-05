e0186860a3d4c830b4fb83c7a931180d9e2f56ee
Amph <amprubio@ucm.es>
2020-03-05 15:01:13 +0100

    inicio opciones + botoncicos para los ajustes

--- src/MF-FighterTraighter/MF-FighterTraighter/Options.cpp
+++ src/MF-FighterTraighter/MF-FighterTraighter/Options.cpp
@@ -1,7 +1,18 @@
 #include "Options.h"
+#include <iostream>
+#include "Entity.h"
+#include "InputManager.h"
 
+
+#include "RenderImage.h"
+#include "Transform.h"
+#include "Button.h"
+
+#include "App.h"
+#include "consts.h"
 Options::Options(App* app): GameState(app)
 {
+	std::cout << "Options";
 }
 
 Options::~Options()
@@ -10,6 +21,19 @@ Options::~Options()
 
 void Options::init()
 {
+	Entity* options_logo=new Entity();
+
+	Transform* t_logo = options_logo->addComponent<Transform>(); 
+
+	t_logo->setPosition(WINDOW_WIDTH_ / 2, 200);
+
+	Entity* fullscreen = new Entity();
+	Entity* audiovolume= new Entity(); //boton que se deslizará
+	Entity* botonmas = new Entity();
+	Entity* botonmenos = new Entity();
+
+
+	Entity* backtomenu=new Entity;
 }
 
 void Options::update()
--- src/MF-FighterTraighter/MF-FighterTraighter/Options.h
+++ src/MF-FighterTraighter/MF-FighterTraighter/Options.h
@@ -1,5 +1,12 @@
 #pragma once
 #include "GameState.h"
+
+class App;
+//callbacks para las opciones
+
+void fullScreenCallback(App* app);
+void SetResolution(App* app);
+
 class Options: public GameState
 {
 public:
@@ -11,5 +18,10 @@ public:
 	virtual void render() override;
 
 private:
+
+	//by the moment for changing resolutions
+	Entity* fullscreen;
+	Entity* audiovolume;
+	Entity* backtomenu;
 };
 
