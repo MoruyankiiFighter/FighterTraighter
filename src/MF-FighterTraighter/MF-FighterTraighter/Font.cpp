caf2a8d16c7ca757d9a99276f75b0f0f63ca3322
Eloy Moreno <eloymore@ucm.es>
2020-03-09 18:57:48 +0100

    Terminada clase Font.

--- src/MF-FighterTraighter/MF-FighterTraighter/Font.cpp
+++ src/MF-FighterTraighter/MF-FighterTraighter/Font.cpp
@@ -1,13 +1,32 @@
 #include "Font.h"
 
-Font::Font() : font(nullptr)
+Font::Font() : font(nullptr), fontSize_(), path_()
 {
+}
+
+void Font::setFontSize(int size)
+{
+	if (fontSize_ != size) {
+		ClearFont();
+		Loadfont(path_, size);
+	}
+}
+
+void Font::changeFont(std::string path)
+{
+	if (path_ != path) {
+		ClearFont();
+		Loadfont(path, fontSize_);
+	}
 }
 
 void Font::Loadfont(std::string path, int size)
 {
-	if (font == nullptr) ClearFont();
-	TTF_OpenFont(path.c_str(), size);
+	if (font != nullptr) ClearFont();
+	font = TTF_OpenFont(path.c_str(), size);
+	if (font == nullptr) throw "Error"; // CHANGE TO PROPER EXCEPTION
+	path_ = path;
+	fontSize_ = size;
 }
 
 void Font::ClearFont()
--- src/MF-FighterTraighter/MF-FighterTraighter/Font.h
+++ src/MF-FighterTraighter/MF-FighterTraighter/Font.h
@@ -6,11 +6,19 @@ class Font
 {
 public:
 	Font();
+	Font(const Font&) = delete;
+	Font operator=(const Font&) = delete;
 	Font(std::string path, int file) { Loadfont(path, file); }
+
+	void setFontSize(int size);
+	void changeFont(std::string path);
 	void Loadfont(std::string path, int size);
+
 	void ClearFont();
 	virtual ~Font();
 protected:
 	TTF_Font* font;
+	int fontSize_;
+	std::string path_;
 };
 
