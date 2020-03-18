4510d41d371eda4ab59711f5db0be4a0e79b9720
Amph <amprubio@ucm.es>
2020-03-18 19:48:09 +0100

    Update IndexSlider.cpp

--- src/MF-FighterTraighter/MF-FighterTraighter/IndexSlider.cpp
+++ src/MF-FighterTraighter/MF-FighterTraighter/IndexSlider.cpp
@@ -1,9 +1,12 @@
 #include "IndexSlider.h"
-
+#include "Entity.h"
 void IndexSlider::init()
 {
+	transform_ = entity_->getComponent<Transform>(ecs::Transform);
 }
 
 void IndexSlider::update()
 {
+	transform_->setPosition(transform_slider->getPosition().getX(), transform_->getPosition().getY());
 }
+
