92e399d04143906c8d08597fea5bfd7a8ec36de1
Amph <amprubio@ucm.es>
2020-03-23 17:16:29 +0100

    limpieza 1/??

--- src/MF-FighterTraighter/MF-FighterTraighter/SDLDebugDraw.h
+++ src/MF-FighterTraighter/MF-FighterTraighter/SDLDebugDraw.h
@@ -11,14 +11,7 @@ public:
 	SDLDebugDraw(SDL_Renderer* r) :  b2Draw(), renderer(r) {}
 	//debug de hitbox de box2D, solo usar drawPoligon si van a ser poligonos, las demas hacen nada
 	/// Dibuja la hitbox
-	void DrawPolygon(const b2Vec2* vertices, int32 vertexCount, const b2Color& color){
-
-		SDL_SetRenderDrawColor(renderer, 0, 0, 255, SDL_ALPHA_OPAQUE);
-		for (int32 i = 0; i < vertexCount; i++) {
-			SDL_RenderDrawLine(renderer, vertices[i].x, vertices[i].y, vertices[(i + 1) % vertexCount].x, vertices[(i + 1) % vertexCount].y);
-		}
-		SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-	}
+	void DrawPolygon(const b2Vec2* vertices, int32 vertexCount, const b2Color& color);
 	void DrawSolidPolygon(const b2Vec2* vertices, int32 vertexCount, const b2Color& color)override {}
 	void DrawCircle(const b2Vec2& center, float32 radius, const b2Color& color)override {}
 	void DrawSolidCircle(const b2Vec2& center, float32 radius, const b2Vec2& axis, const b2Color& color) {}
