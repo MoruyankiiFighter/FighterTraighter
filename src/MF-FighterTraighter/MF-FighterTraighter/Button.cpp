5fa82952e660ec56d82753b1d1c0f64eb26b8fc3
Eloy Moreno <eloymore@ucm.es>
2020-03-28 18:55:41 +0100

    Los botones cambian visualmente al ser seleccionados y pulsados.

--- src/MF-FighterTraighter/MF-FighterTraighter/Button.cpp
+++ src/MF-FighterTraighter/MF-FighterTraighter/Button.cpp
@@ -17,11 +17,13 @@ void Button::handleInput()
 		if (imngr->pressedAccept()) {
 			if (!pressed_) {
 				pressed_ = true;
+				entity_->getComponent<RenderImage>(ecs::RenderImage)->setFrame(2, 0);
 				if (clickCallback_) clickCallback_(app_);
 			}
 		}
 		else if (pressed_) {
 			pressed_ = false;
+			entity_->getComponent<RenderImage>(ecs::RenderImage)->setFrame(0, 0);
 			if (stopClickCallback_) stopClickCallback_(app_);
 		}
 	}
--- src/MF-FighterTraighter/MF-FighterTraighter/Button.h
+++ src/MF-FighterTraighter/MF-FighterTraighter/Button.h
@@ -14,7 +14,12 @@ public:
 	Button(CallBackOnClick* startClickCallback = nullptr, CallBackOnClick* stopClickCallback = nullptr) : Component(ecs::Button), clickCallback_(startClickCallback), stopClickCallback_(stopClickCallback) {};
 	inline void setClickCallback(CallBackOnClick* callback) { clickCallback_ = callback; }
 	inline void setStopClickCallback(CallBackOnClick* callback) { stopClickCallback_ = callback; }
-	inline void setSelected(bool sel) { selected_ = sel; }
+	inline void setSelected(bool sel) 
+	{
+		selected_ = sel; 
+		if(sel) entity_->getComponent<RenderImage>(ecs::RenderImage)->setFrame(1, 0);
+		else entity_->getComponent<RenderImage>(ecs::RenderImage)->setFrame(0, 0);
+	}
 	//destructor
 	virtual ~Button() {};
 
