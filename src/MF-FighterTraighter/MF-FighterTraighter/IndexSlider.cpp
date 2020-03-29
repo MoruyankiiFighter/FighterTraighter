3c94aa60395628f1e2c5589880234533cf0ad036
Eloy Moreno <eloymore@ucm.es>
2020-03-29 01:11:55 +0100

    Reajustado IndexSlider con respecto al slider.

--- src/MF-FighterTraighter/MF-FighterTraighter/IndexSlider.cpp
+++ src/MF-FighterTraighter/MF-FighterTraighter/IndexSlider.cpp
@@ -15,6 +15,7 @@ void IndexSlider::update()
 	double sliderValue = slider_->getValue();
 	double sliderWidth = transform_slider->getWidth() * transform_slider->getWMult();
 	double sliderRange = slider_->getMaxValue() - slider_->getMinValue();
-	transform_->setPosition({ transform_slider->getPosition().getX() + ((sliderValue - slider_->getMinValue()) / sliderRange) * sliderWidth, transform_slider->getPosition().getY() - transform_->getHeight() * transform_->getHMult() / 2 });
+	transform_->setPosition({ transform_slider->getPosition().getX() + ((sliderValue - slider_->getMinValue()) / sliderRange) * sliderWidth - transform_->getWidth() * transform_->getWMult() / 2, 
+		transform_slider->getPosition().getY() - transform_->getHeight() * transform_->getHMult() / 2 });
 }
 
