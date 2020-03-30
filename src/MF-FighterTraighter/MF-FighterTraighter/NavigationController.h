3bf1cc58fc098a5c98a21227ce542a6e92f284a7
Amph <amprubio@ucm.es>
2020-03-30 11:43:35 +0100

    diagrama de clases

--- src/MF-FighterTraighter/MF-FighterTraighter/NavigationController.h
+++ src/MF-FighterTraighter/MF-FighterTraighter/NavigationController.h
@@ -8,11 +8,14 @@ class NavigationController :
 {
 public:
 	NavigationController(size_t sizeX, size_t sizeY);
+	virtual ~NavigationController() {};
+
 	void init() override;
 	void handleInput() override;
-	virtual ~NavigationController() {};
+	
 	void SetElementInPos(Entity* ent, size_t x, size_t y);
 	Entity* GetElementInPos(size_t x, size_t y);
+
 protected:
 	LinkedGrid<Entity*> grid_;
 	int cursorPositionX_, cursorPositionY_;
