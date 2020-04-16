f699ee6c4f61de1affa4097d33f06a87993c6139
Sergio Alberto Luis Cano <serluis@ucm.es>
2020-04-16 11:35:11 +0200

    agregado diccionario

--- src/MF-FighterTraighter/MF-FighterTraighter/AssetsManager.h
+++ src/MF-FighterTraighter/MF-FighterTraighter/AssetsManager.h
@@ -1,14 +1,68 @@
 #pragma once
 #include "SDL.h"
 #include "SDL_image.h"
-#include <vector>
+#include <map>
 #include "Texture.h"
 #include "Font.h"
+#include <SDL_mixer.h>
 class App;
 
 class AssetsManager
 {
 public:
+	enum TextureNames {
+		Player,
+		Button,
+		Logo,
+		Controller,
+		Slidebar,
+		SliderRegulator,
+		PlayerActing,
+		Background,
+		Healthbar,
+		HealthbarBack,
+		RoundCounter
+	};
+
+	enum FontNames {
+		Roboto_Black
+	};
+
+	enum MusicNames {
+		MENU_PRINCIPAL,
+		MENU_OPCIONES,
+		FIGHT_1,
+		FIGHT_2,
+		FIGHT_3,
+		FIGHT_4
+	};
+
+	enum SFXNames {
+		/// Taunts
+		AISHA_1,
+		AISHA_2,
+		AISHA_3,
+		AISHA_4,
+		AISHA_5,
+		AISHA_6,
+
+		FLOR_1,
+		FLOR_2,
+		FLOR_3,
+		
+		MKWOP_1,
+		MKWOP_2,
+		MKWOP_3,
+		MKWOP_4,
+		MKWOP_5,
+		
+		MKBIRD_1,
+		MKBIRD_2,
+		MKBIRD_3,
+		/// SFX
+
+	};
+
 	//constructors
 	AssetsManager(App* app);
 	AssetsManager(AssetsManager&) = delete;
@@ -19,42 +73,31 @@ public:
 
 
 	//texture methods
-	//return the texture with the position id from the textures_ vector 
-	Texture* getTexture(size_t id);
+	//return the texture with the position id from the textures_ map 
+	Texture* getTexture(AssetsManager::TextureNames id);
 	void loadTextures(); // Textures are hardcoded for now
 
 	//font methods
-	//return the font with the position id from the fonts_ vector
-	Font* getFont(size_t id);
+	//return the font with the position id from the fonts_ map
+	Font* getFont(AssetsManager::FontNames id);
 	void loadFonts(); // Fonts are hardcoded for now
 
-	//audio methods
 
-	//return the audio with the position id
-	//Audio* getAudio(size_t id);
+	//music methods
+	Mix_Music* getMusic(AssetsManager::MusicNames id);
 	void loadMusic();
 
-	void playMusic(const string& name, int channel);
-	void resumeAll();
-	void resumeMusic();
-	void pauseMusic();
-	void stopMusic();
-
-	int getGeneralVolume() const;
-	int getMusicVolume() const;
-	int getChannelVolume(int channel) const;
-	void setGeneralVolume(float volume_ratio);
-	void setChannelvolume(int channel, float volume_ratio);
-	void setMusicVolume(float volume_ratio);
-	void setSFXVolume(const string& name, float volume_ratio);
+	
+	//sfx methods
+	Mix_Chunk* getSFX(AssetsManager::SFXNames id);
+	void loadSFX();
 
 
 private:
-	std::vector<Texture*> textures_;
-	std::vector<Font*> fonts_;
-	//std::vector<Audio*> music_;
-	//std::vector<Audio*> sfx_;
-
+	std::map <TextureNames,Texture*> textures_;	//images/Textures
+	std::map <FontNames,Font*> fonts_;			//fonts
+	std::map <MusicNames,Mix_Music*> music_;	//music
+	std::map <SFXNames,Mix_Chunk*> sfx_;		//sound effects
 	App* app_;
 };
 
