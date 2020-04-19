ad4f5425d597fad87f5914b5eb0953fce07b9c34
Sergio Alberto Luis Cano <serluis@ucm.es>
2020-04-19 17:57:44 +0200

    Creados botones de skill menu

--- src/MF-FighterTraighter/MF-FighterTraighter/SkillSelection.cpp
+++ src/MF-FighterTraighter/MF-FighterTraighter/SkillSelection.cpp
@@ -5,13 +5,25 @@
 
 void SkillSelection::init()
 {
+	// Fondo?
 
-	UIFactory::createPanel(app_, this, app_->getAssetsManager()->getTexture(AssetsManager::SubMenuFondo), Vector2D(100, 100), (app_->getWindowManager()->getCurResolution().w / 2) - 100, (app_->getWindowManager()->getCurResolution().h) - 100, 0);
+	// Paneles de fondo
 
-	UIFactory::createPanel(app_, this, app_->getAssetsManager()->getTexture(AssetsManager::SubMenuFondo), Vector2D((app_->getWindowManager()->getCurResolution().w) + 100, 100), (app_->getWindowManager()->getCurResolution().w / 2) - 100, (app_->getWindowManager()->getCurResolution().h) - 100, 0);
+	UIFactory::createPanel(app_, this, app_->getAssetsManager()->getTexture(AssetsManager::SubMenuFondo), 
+		Vector2D(50, 50), (app_->getWindowManager()->getCurResolution().w / 2) - 100, (app_->getWindowManager()->getCurResolution().h) - 100, 0);
 
+	UIFactory::createPanel(app_, this, app_->getAssetsManager()->getTexture(AssetsManager::SubMenuFondo), 
+		Vector2D((app_->getWindowManager()->getCurResolution().w / 2) + 50, 50), (app_->getWindowManager()->getCurResolution().w / 2) - 100, (app_->getWindowManager()->getCurResolution().h) - 100, 0);
 
+	// boton de cambio de estado
 
+	UIFactory::createButton(app_, this, app_->getAssetsManager()->getTexture(AssetsManager::Button), app_->getAssetsManager()->getFont(AssetsManager::Roboto_Black),
+		Vector2D(0, 0), Vector2D((((app_->getWindowManager()->getCurResolution().w / 2) / 2)) - 150, ((app_->getWindowManager()->getCurResolution().h) - 225)), Vector2D(0, 75),
+		300, 75, 0, nullptr, nullptr, "Continue", 25, TextComponent::TextAlignment::Center);
+
+	UIFactory::createButton(app_, this, app_->getAssetsManager()->getTexture(AssetsManager::Button), app_->getAssetsManager()->getFont(AssetsManager::Roboto_Black),
+		Vector2D(0, 0), Vector2D((((app_->getWindowManager()->getCurResolution().w / 4) * 3)) - 150, ((app_->getWindowManager()->getCurResolution().h) - 225)), Vector2D(0, 150),
+		300, 75, 0, nullptr, nullptr, "Continue2", 25, TextComponent::TextAlignment::Center);
 }
 
 void SkillSelection::handleInput()
