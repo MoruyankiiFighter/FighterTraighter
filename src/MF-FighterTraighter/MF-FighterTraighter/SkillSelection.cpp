6ff3b41789413c6c6f0c44856136d84257ddc201
Amph <amprubio@ucm.es>
2020-04-19 17:06:39 +0100

    Update SkillSelection.cpp

--- src/MF-FighterTraighter/MF-FighterTraighter/SkillSelection.cpp
+++ src/MF-FighterTraighter/MF-FighterTraighter/SkillSelection.cpp
@@ -17,13 +17,25 @@ void SkillSelection::init()
 
 	// boton de cambio de estado
 
-	UIFactory::createButton(app_, this, app_->getAssetsManager()->getTexture(AssetsManager::Button), app_->getAssetsManager()->getFont(AssetsManager::Roboto_Black),
-		Vector2D(0, 0), Vector2D((((app_->getWindowManager()->getCurResolution().w / 2) / 2)) - 150, ((app_->getWindowManager()->getCurResolution().h) - 225)), Vector2D(0, 75),
-		300, 75, 0, nullptr, nullptr, "Continue", 25, TextComponent::TextAlignment::Center);
 
-	UIFactory::createButton(app_, this, app_->getAssetsManager()->getTexture(AssetsManager::Button), app_->getAssetsManager()->getFont(AssetsManager::Roboto_Black),
-		Vector2D(0, 0), Vector2D((((app_->getWindowManager()->getCurResolution().w / 4) * 3)) - 150, ((app_->getWindowManager()->getCurResolution().h) - 225)), Vector2D(0, 150),
-		300, 75, 0, nullptr, nullptr, "Continue2", 25, TextComponent::TextAlignment::Center);
+	tuple < Entity*, Entity*> options = UIFactory::createButton(app_, this, app_->getAssetsManager()->getTexture(AssetsManager::Button), app_->getAssetsManager()->getFont(AssetsManager::Roboto_Black),
+		Vector2D(-300, 300), Vector2D(app_->getWindowManager()->getCurResolution().w / 2, app_->getWindowManager()->getCurResolution().h / 2),
+		Vector2D(250, 75),
+		500, 150, 0, nullptr, nullptr, "Continue", 150, TextComponent::TextAlignment::Center);
+
+	tuple < Entity*, Entity*> exit = UIFactory::createButton(app_, this, app_->getAssetsManager()->getTexture(AssetsManager::Button), app_->getAssetsManager()->getFont(AssetsManager::Roboto_Black),
+		Vector2D(300, 300), Vector2D(app_->getWindowManager()->getCurResolution().w / 2, app_->getWindowManager()->getCurResolution().h / 2),
+		Vector2D(250, 75),
+		500, 150, 0, nullptr, nullptr, "Acept", 150, TextComponent::TextAlignment::Center);
+
+
+	//UIFactory::createButton(app_, this, app_->getAssetsManager()->getTexture(AssetsManager::Button), app_->getAssetsManager()->getFont(AssetsManager::Roboto_Black),
+	//	Vector2D(0, 0), Vector2D((((app_->getWindowManager()->getCurResolution().w / 2) / 2)) - 150, ((app_->getWindowManager()->getCurResolution().h) - 225)), Vector2D(0, 75),
+	//	300, 75, 0, nullptr, nullptr, "Continue", 25, TextComponent::TextAlignment::Center);
+
+	//UIFactory::createButton(app_, this, app_->getAssetsManager()->getTexture(AssetsManager::Button), app_->getAssetsManager()->getFont(AssetsManager::Roboto_Black),
+	//	Vector2D(0, 0), Vector2D((((app_->getWindowManager()->getCurResolution().w / 4) * 3)) - 150, ((app_->getWindowManager()->getCurResolution().h) - 225)), Vector2D(0, 150),
+	//	300, 75, 0, nullptr, nullptr, "Continue2", 25, TextComponent::TextAlignment::Center);
 }
 
 void SkillSelection::handleInput()
