055765050883fc968980fedeeafa4e2a91ad9223
Daniel Cortijo <dcortijo@ucm.es>
2020-04-28 13:20:29 +0200

    Ajustadas animaciones y hitbox de AHK de Mk
    Falta todavía rompeguardia y "casteando" (uilizando habilidades), que habrá que poner en los estados

--- src/MF-FighterTraighter/MF-FighterTraighter/MkWH00PData.cpp
+++ src/MF-FighterTraighter/MF-FighterTraighter/MkWH00PData.cpp
@@ -3,7 +3,7 @@
 MkWH00PData::MkWH00PData(std::vector<SDL_Scancode> keys, double width, double height, double rotation, double jump_impulse, Vector2D ini_pos, double speed, double ini_health, double attack, double defense, int playerNumber) :
 	PlayerData(keys, width, height, rotation, jump_impulse, ini_pos, speed, ini_health, attack, defense, playerNumber) {
 	animLength_ = { {4, true, 12}, {4, true, 15}, {2, true, 3}, {1, true, 15}, {4, false, 2}, {12, false, 10}, {7, false, 10}, {9, false, 8},
-	{15, false, 7}, {7, false, 13}, {9, false, 10}, {10, false, 7}, {4, true, 15}, {2, true, 15}, {2, false, 10}, {3, true, 4}, {2, false, 10}, 
+	{15, false, 7}, {7, false, 13}, {9, false, 10}, {10, false, 7}, {5, false, 15}, {2, true, 15}, {2, false, 10}, {3, true, 4}, {2, false, 10}, 
 	{2, false, 3}, {4, true, 12}, {2, false, 7}, {2, false, 7}, {2, true, 15} };
 }
 
@@ -310,7 +310,7 @@ void MkWH00PData::AHK1(Entity* ent)
 		{ (double)orientation_ * hitbox_X, ahk1.position.getY() }, ahk1.width, ahk1.height, ahk1.time, pD->getAttack() * ahk1.damage, ahk1.hitstun, { (double)orientation_ * ahk1.knockBack.getX(), ahk1.knockBack.getY() }, pT->getBody(), pD->getPlayerNumber(), pT->getCategory(), pT->getMask());	
 }
 PlayerData::CallbackData MkWH00PData::ahk1 = PlayerData::CallbackData{
-	{ 50, -75 },
+	{ 50, -50 },
 	{ 400, 0 },
 	250,
 	200,
