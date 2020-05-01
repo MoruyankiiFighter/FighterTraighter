99513e15fa21125eaa90b2448ad6b8dbb4a95c6e
Amph <amprubio@ucm.es>
2020-05-01 18:34:04 +0100

    Update CharacterSelectionLogic.h

--- src/MF-FighterTraighter/MF-FighterTraighter/CharacterSelectionLogic.h
+++ src/MF-FighterTraighter/MF-FighterTraighter/CharacterSelectionLogic.h
@@ -8,17 +8,18 @@ public:
 
 	CharacterSelectionLogic(TextComponent* j1_desc, string aisha_desc, string mkwhoop_desc, string flor_desc, string mock_desc, NavigationController* nav, UIElement* aisha, UIElement* flor, UIElement* mkwhoop, UIElement* mock  ) : Component(ecs::CharacterSelectionLogic),
 		desc_(j1_desc), aisha_(aisha),aisha_desc_(aisha_desc),flor_(flor), flor_desc_(flor_desc),mock_(mock),mock_desc_(mock_desc),mkwhoop_(mkwhoop),mkwhoop_desc_(mkwhoop_desc),nav_(nav) { }
+	
 	void init() override;
 	void update() override;
-
+	
 	void SetElements();
 private:
 	TextComponent* desc_=nullptr;
 	string aisha_desc_, flor_desc_, mkwhoop_desc_, mock_desc_;
-	NavigationController* nav_;
-	UIElement* aisha_;
-	UIElement* flor_;
-	UIElement* mkwhoop_;
-	UIElement* mock_;
+	NavigationController* nav_=nullptr;
+	UIElement* aisha_=nullptr;
+	UIElement* flor_=nullptr;
+	UIElement* mkwhoop_=nullptr;
+	UIElement* mock_=nullptr;
 };
 
