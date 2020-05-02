bd640393ab9ffcae9cb6026396ba51139eb862fd
Amph <amprubio@ucm.es>
2020-05-02 18:16:30 +0100

    limpieza

--- src/MF-FighterTraighter/MF-FighterTraighter/CharacterSelection.cpp
+++ src/MF-FighterTraighter/MF-FighterTraighter/CharacterSelection.cpp
@@ -22,45 +22,81 @@ void CharacterSelection::init()
 	//panel
 
 	Entity* centralP_ = UIFactory::createPanel(app_, this, app_->getAssetsManager()->getTexture(AssetsManager::Panel),
-		Vector2D(ancho / 8.5, 50), Vector2D(ancho /5, 50), Vector2D(ancho / 5, 50), (ancho / 20)*16, (alto/15)*10, 0);
-
+		Vector2D(app_->getWindowManager()->getCurResolution().w / 8.5, 50), Vector2D(app_->getWindowManager()->getCurResolution().w /5, 50), 
+		Vector2D(app_->getWindowManager()->getCurResolution().w / 5, 50), (app_->getWindowManager()->getCurResolution().w / 20)*16, 
+		(app_->getWindowManager()->getCurResolution().h /15)*10, 0);
+	
+	//artwork of the character
 	Entity* leftP_ = UIFactory::createPanel(app_, this, app_->getAssetsManager()->getTexture(AssetsManager::Banner),
-		Vector2D((ancho / 25) *1, 100), Vector2D((ancho / 25) * 1, 100), Vector2D((ancho / 25) * 1, 100), (ancho / 30) *6, (alto/15)*10, 0);
+		Vector2D((app_->getWindowManager()->getCurResolution().w / 25) *1, 100), Vector2D((app_->getWindowManager()->getCurResolution().w / 25) * 1, 100), 
+		Vector2D((app_->getWindowManager()->getCurResolution().w / 25) * 1, 100), (app_->getWindowManager()->getCurResolution().w / 30) *6, (app_->getWindowManager()->getCurResolution().h /15)*10, 0);
 
 	Entity* rightP_ = UIFactory::createPanel(app_, this, app_->getAssetsManager()->getTexture(AssetsManager::Banner),
-		Vector2D((ancho / 25) * 20.5, 100), Vector2D((ancho / 25) * 19.5, 100), Vector2D((ancho / 25) * 19.5, 100), (ancho / 30) * 4.5, (alto / 15) * 10, 0);
+		Vector2D((app_->getWindowManager()->getCurResolution().w / 25) * 20.5, 100),
+		Vector2D((app_->getWindowManager()->getCurResolution().w / 25) * 19.5, 100), Vector2D((app_->getWindowManager()->getCurResolution().w / 25) * 19.5, 100),
+		(app_->getWindowManager()->getCurResolution().w / 30) * 4.5, (app_->getWindowManager()->getCurResolution().h / 15) * 10, 0);
 
 	// buttons
 	tuple <Entity*, Entity*> boton1 = UIFactory::createButton(app_, this, app_->getAssetsManager()->getTexture(AssetsManager::Button), app_->getAssetsManager()->getFont(AssetsManager::Roboto_Black),
-		Vector2D((ancho / 25) * 1, (alto / 14) * 11.5), Vector2D((ancho / 25) * 1, (alto / 14) * 11.5), Vector2D((ancho / 25) * 1, (alto / 14) * 11.5), (ancho / 25) * 4, (alto / 25) * 4, 0, nullptr, nullptr, "Continue", letra / 2, TextComponent::TextAlignment::Center);
+		Vector2D((app_->getWindowManager()->getCurResolution().w / 25) * 1, (app_->getWindowManager()->getCurResolution().h / 14) * 11.5),
+		Vector2D((app_->getWindowManager()->getCurResolution().w / 25) * 1, (app_->getWindowManager()->getCurResolution().h / 14) * 11.5),
+		Vector2D((app_->getWindowManager()->getCurResolution().w / 25) * 1, (app_->getWindowManager()->getCurResolution().h / 14) * 11.5),
+		(app_->getWindowManager()->getCurResolution().w / 25) * 4, (app_->getWindowManager()->getCurResolution().h / 25) * 4, 0,
+		nullptr, nullptr, "Continue", letra / 2, TextComponent::TextAlignment::Center);
+
 	std::get<1>(boton1)->getComponent<UITransform>(ecs::Transform)->Center;
+
 	tuple <Entity*, Entity*> boton2 = UIFactory::createButton(app_, this, app_->getAssetsManager()->getTexture(AssetsManager::Button), app_->getAssetsManager()->getFont(AssetsManager::Roboto_Black),
-		Vector2D((ancho / 25) * 20, (alto / 14) * 11.5), Vector2D((ancho / 25) * 20, (alto / 14) * 11.5), Vector2D((ancho / 25) * 20, (alto / 14) * 11.5), (ancho / 25) * 4, (alto / 25) * 4, 0, nullptr, nullptr, "Continue", letra / 2, TextComponent::TextAlignment::Center);
+		Vector2D((app_->getWindowManager()->getCurResolution().w / 25) * 20, (app_->getWindowManager()->getCurResolution().h / 14) * 11.5),
+		Vector2D((app_->getWindowManager()->getCurResolution().w / 25) * 20, (app_->getWindowManager()->getCurResolution().h / 14) * 11.5),
+		Vector2D((app_->getWindowManager()->getCurResolution().w / 25) * 20, (app_->getWindowManager()->getCurResolution().h / 14) * 11.5),
+		(app_->getWindowManager()->getCurResolution().w / 25) * 4, (app_->getWindowManager()->getCurResolution().h / 25) * 4,
+		0, nullptr, nullptr, "Continue", letra / 2, TextComponent::TextAlignment::Center);
+
 	std::get<1>(boton1)->getComponent<UITransform>(ecs::Transform)->Center;
 	
 	//icons of the character
 	tuple <Entity*, Entity*> flor_ = UIFactory::createButton(app_, this, app_->getAssetsManager()->getTexture(AssetsManager::Flor_icon), app_->getAssetsManager()->getFont(AssetsManager::Roboto_Black),
-		Vector2D((ancho / 25) * 6, (alto / 14) * 2), Vector2D((ancho / 25) * 6, (alto / 14) * 2), Vector2D((ancho / 25) * 6, (alto / 14) * 2), (ancho / 25) * 3, (alto / 14) * 3 ,0,
+		Vector2D((app_->getWindowManager()->getCurResolution().w / 25) * 6, (app_->getWindowManager()->getCurResolution().h / 14) * 2),
+		Vector2D((app_->getWindowManager()->getCurResolution().w / 25) * 6, (app_->getWindowManager()->getCurResolution().h / 14) * 2),
+		Vector2D((app_->getWindowManager()->getCurResolution().w / 25) * 6, (app_->getWindowManager()->getCurResolution().h / 14) * 2),
+		(app_->getWindowManager()->getCurResolution().w / 25) * 3, (app_->getWindowManager()->getCurResolution().h / 14) * 3 ,0,
 		nullptr, nullptr, " ", letra, TextComponent::TextAlignment::Center);
+
 	tuple <Entity*, Entity*> mkwhoop_ = UIFactory::createButton(app_, this, app_->getAssetsManager()->getTexture(AssetsManager::Ganonbot_icon), app_->getAssetsManager()->getFont(AssetsManager::Roboto_Black),
-		Vector2D((ancho / 25) * 9.5, (alto / 14) * 2), Vector2D((ancho / 25) * 9.5, (alto / 14) * 2), Vector2D((ancho / 25) * 9.5, (alto / 14) * 2), (ancho / 25) * 3, (alto / 14) * 3, 0,
+		Vector2D((app_->getWindowManager()->getCurResolution().w / 25) * 9.5, (app_->getWindowManager()->getCurResolution().h / 14) * 2),
+		Vector2D((app_->getWindowManager()->getCurResolution().w / 25) * 9.5, (app_->getWindowManager()->getCurResolution().h / 14) * 2),
+		Vector2D((app_->getWindowManager()->getCurResolution().w / 25) * 9.5, (app_->getWindowManager()->getCurResolution().h / 14) * 2),
+		(app_->getWindowManager()->getCurResolution().w / 25) * 3, (app_->getWindowManager()->getCurResolution().h / 14) * 3, 0,
 		nullptr, nullptr, " ", letra, TextComponent::TextAlignment::Center);
+
 	tuple <Entity*, Entity*> aisha_ = UIFactory::createButton(app_, this, app_->getAssetsManager()->getTexture(AssetsManager::Aisha_icon), app_->getAssetsManager()->getFont(AssetsManager::Roboto_Black),
-		Vector2D((ancho / 25) * 13, (alto / 14) * 2), Vector2D((ancho / 25) * 13, (alto / 14) * 2), Vector2D((ancho / 25) * 13, (alto / 14) * 2), (ancho / 25) * 3, (alto / 14) * 3, 0,
+		Vector2D((app_->getWindowManager()->getCurResolution().w / 25) * 13, (app_->getWindowManager()->getCurResolution().h / 14) * 2),
+		Vector2D((app_->getWindowManager()->getCurResolution().w / 25) * 13, (app_->getWindowManager()->getCurResolution().h / 14) * 2),
+		Vector2D((app_->getWindowManager()->getCurResolution().w / 25) * 13, (app_->getWindowManager()->getCurResolution().h / 14) * 2),
+		(app_->getWindowManager()->getCurResolution().w / 25) * 3, (app_->getWindowManager()->getCurResolution().h / 14) * 3, 0,
 		nullptr, nullptr, " ", letra, TextComponent::TextAlignment::Center);
+
 	tuple <Entity*, Entity*> mockinbird_ = UIFactory::createButton(app_, this, app_->getAssetsManager()->getTexture(AssetsManager::Mock_icon), app_->getAssetsManager()->getFont(AssetsManager::Roboto_Black),
-		Vector2D((ancho / 25) * 16.5, (alto / 14) * 2), Vector2D((ancho / 25) * 16.5, (alto / 14) * 2), Vector2D((ancho / 25) * 16.5, (alto / 14) * 2), (ancho / 25) * 3, (alto / 14) * 3, 0,
+		Vector2D((app_->getWindowManager()->getCurResolution().w / 25) * 16.5, (app_->getWindowManager()->getCurResolution().h / 14) * 2),
+		Vector2D((app_->getWindowManager()->getCurResolution().w / 25) * 16.5, (app_->getWindowManager()->getCurResolution().h / 14) * 2),
+		Vector2D((app_->getWindowManager()->getCurResolution().w / 25) * 16.5, (app_->getWindowManager()->getCurResolution().h / 14) * 2),
+		(app_->getWindowManager()->getCurResolution().w / 25) * 3, (app_->getWindowManager()->getCurResolution().h / 14) * 3, 0,
 		nullptr, nullptr, " ", letra, TextComponent::TextAlignment::Center);
 
 
-	//artwork of the character
-
-	Entity* j1_ = entManager_.addEntity();
-	Entity* j2 = entManager_.addEntity();
-	Entity* text_j1 = UIFactory::createText(app_, this, Vector2D((ancho / 25) * 5, (alto / 14) * 7), Vector2D((ancho / 25) * 5, (alto / 14) * 7),
-		Vector2D((ancho / 25) * 5, (alto / 14) * 7), app_->getAssetsManager()->getFont(AssetsManager::Roboto_Black), "description_ j1", 30, (ancho / 25) * 5, (alto / 14) * 2);
-	Entity* text_j2 = UIFactory::createText(app_, this, Vector2D((ancho / 25) * 15, (alto / 14) * 8), Vector2D((ancho / 25) * 15, (alto / 14) * 8),
-		Vector2D((ancho / 25) * 15, (alto / 14) * 8), app_->getAssetsManager()->getFont(AssetsManager::Roboto_Black), "description_ j2", 30, (ancho / 25) * 5, (alto / 14) * 2);
+	
+	Entity* text_j1 = UIFactory::createText(app_, this, Vector2D((app_->getWindowManager()->getCurResolution().w / 25) * 5, (app_->getWindowManager()->getCurResolution().h / 14) * 7),
+		Vector2D((app_->getWindowManager()->getCurResolution().w / 25) * 5, (app_->getWindowManager()->getCurResolution().h / 14) * 7),
+		Vector2D((app_->getWindowManager()->getCurResolution().w / 25) * 5, (app_->getWindowManager()->getCurResolution().h / 14) * 7),
+		app_->getAssetsManager()->getFont(AssetsManager::Roboto_Black), "description_ j1", 30, 
+		(app_->getWindowManager()->getCurResolution().w / 25) * 5, (app_->getWindowManager()->getCurResolution().h / 14) * 2);
+	
+	Entity* text_j2 = UIFactory::createText(app_, this, Vector2D((app_->getWindowManager()->getCurResolution().w / 25) * 15, (app_->getWindowManager()->getCurResolution().h / 14) * 8),
+		Vector2D((app_->getWindowManager()->getCurResolution().w / 25) * 15, (app_->getWindowManager()->getCurResolution().h / 14) * 8),
+		Vector2D((app_->getWindowManager()->getCurResolution().w / 25) * 15, (app_->getWindowManager()->getCurResolution().h / 14) * 8),
+		app_->getAssetsManager()->getFont(AssetsManager::Roboto_Black), "description_ j2", 30, 
+		(app_->getWindowManager()->getCurResolution().w / 25) * 5, (app_->getWindowManager()->getCurResolution().h / 14) * 2);
 
 	//Entitys
 
@@ -74,10 +110,9 @@ void CharacterSelection::init()
 	ctrl->SetElementInPos(std::get<0>(boton1)->getComponent<UIElement>(ecs::UIElement), 1, 1);
 	ctrl->SetElementInPos(std::get<0>(boton1)->getComponent<UIElement>(ecs::UIElement), 2, 1);
 	ctrl->SetElementInPos(std::get<0>(boton1)->getComponent<UIElement>(ecs::UIElement), 3, 1);
+	
 
-	//ctrl->SetElementInPos(std::get<0>(boton2)->getComponent<UIElement>(ecs::UIElement), 1, 1);
 	//logic of that for changing the artwork of each player 
-
 	Entity* logic = entManager_.addEntity();
 	logic->addComponent<CharacterSelectionLogic>(text_j1->getComponent<TextComponent>(ecs::TextComponent),leftP_->getComponent<RenderImage>(ecs::RenderImage),aisha_desc, mkwhoop_desc, flor_desc, mock_desc, nav->getComponent<NavigationController>(ecs::NavigationController), 
 		std::get<0>(aisha_)->getComponent<UIElement>(ecs::UIElement), std::get<0>(flor_)->getComponent<UIElement>(ecs::UIElement), std::get<0>(mkwhoop_)->getComponent<UIElement>(ecs::UIElement), std::get<0>(mockinbird_)->getComponent<UIElement>(ecs::UIElement),
--- src/MF-FighterTraighter/MF-FighterTraighter/CharacterSelection.h
+++ src/MF-FighterTraighter/MF-FighterTraighter/CharacterSelection.h
@@ -8,24 +8,17 @@ public:
 
 	//constructor
 	CharacterSelection(App* app) : GameState(app) { init(); }
-	
+
 	void init() override;
 
 private:
-	double ancho = app_->getWindowManager()->getCurResolution().w, alto = app_->getWindowManager()->getCurResolution().h;
+	
 	int factorLetra = 25;
-	int letra = int(std::sqrt((ancho * ancho) + (alto * alto)) / factorLetra);
-	/*std::vector <std::string> descriptions {" ,
-								   "F10R: Es capaz de atacar desde muy lejos, evitando que sus oponentes se acerquen, tiene el rol de zoner. Tiene una vida media-baja.",
-								   "Aisha: Tiene un playstyle balanceado, ni muy rápido ni muy lento, con movimientos que brillan sobre todo en el aire. Tiene una vida media.",
-								   "MockingBird: Sus ataques son rápidos pero de poco daño, promoviendo un playstyle de hacer combos mezclándolos. Tiene una vida baja." };*/
-
+	int letra = 120;
+	
 	string mkwhoop_desc= "MKW00P: \n  Tiene un playstyle lento pero poderoso, \n con golpes contundentes y de alto empuje.\n Tiene una vida alta.";
-
 	string aisha_desc= "Aisha: \n Tiene un playstyle balanceado, ni muy rápido ni muy lento,\n con movimientos que brillan sobre todo en el aire. \n Tiene una vida media.";
-
 	string flor_desc= "F10R: \n Es capaz de atacar desde muy lejos, \n evitando que sus oponentes se acerquen, tiene el rol de zoner. \n Tiene una vida media-baja.";
-
 	string mock_desc = "MockingBird: \n Sus ataques son rápidos pero de poco daño, \n promoviendo un playstyle de hacer combos mezclándolos. \n Tiene una vida baja.";
 
 };
