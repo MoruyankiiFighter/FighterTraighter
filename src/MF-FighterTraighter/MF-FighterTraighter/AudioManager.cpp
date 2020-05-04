16fb492539ddcb490fdf424ab3f0816cd201ac54
ancues02 <ancues02@ucm.es>
2020-05-04 18:21:41 +0200

    añadidos audiosManagers

---
+++ src/MF-FighterTraighter/MF-FighterTraighter/AudioManager.cpp
@@ -0,0 +1,93 @@
+#include "AudioManager.h"
+#include <SDL_mixer.h>
+#include <SDL.h>
+
+AudioManager::AudioManager() : AudioManager(8) { }
+
+AudioManager::AudioManager(int channels) : channels_(channels)
+{
+	if (SDL_Init(SDL_INIT_AUDIO) == 0)
+		if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) != 0)
+			std::cout << "Audio Manager not loaded" << Mix_GetError() << std::endl;
+
+	initialized_ = true;
+}
+
+AudioManager::~AudioManager()
+{
+	Mix_CloseAudio();
+}
+
+Mix_Chunk* AudioManager::loadSFX(const std::string& fileName)
+{
+	Mix_Chunk* sfx;
+	sfx = Mix_LoadWAV(fileName.c_str());
+	return sfx;
+}
+
+void AudioManager::playSFX(Mix_Chunk* sound, int loops, int channel)
+{
+	if (sound != nullptr && loops != -1)
+		Mix_PlayChannel(channel, sound, loops);
+}
+
+Mix_Music* AudioManager::loadMusic(const std::string& fileName)
+{
+	Mix_Music* m = Mix_LoadMUS(fileName.c_str());
+	return m;
+}
+
+void AudioManager::playMusic(Mix_Music* music, bool loops)
+{
+	if (music != nullptr) {
+		Mix_PlayMusic(music, loops ? -1 : 0); //si loop= true se loopea(-1), en caso contrario 1 vez
+	}
+	else {
+		//lanzar excepciones
+	}
+}
+
+int AudioManager::setMusicVolume(int volume)
+{
+	return Mix_VolumeMusic(volume);
+}
+
+void AudioManager::pauseMusic()
+{
+	Mix_PauseMusic();
+}
+
+void AudioManager::resumeMusic()
+{
+	Mix_ResumeMusic();
+}
+
+void AudioManager::resumeAll()
+{
+	Mix_Resume(-1);
+}
+
+void AudioManager::stopMusic()
+{
+	Mix_HaltMusic();
+}
+
+void AudioManager::setGeneralVolume(float MaxVolume, float volume_ratio)
+{
+	Mix_Volume(-1, MaxVolume * volume_ratio);
+}
+
+int AudioManager::getGeneralVolume() const
+{
+	return Mix_Volume(-1, 1);
+}
+
+int AudioManager::getMusicVolume() const
+{
+	return Mix_VolumeMusic(-1);
+}
+
+int AudioManager::getChannelVolume(int channel) const
+{
+	return Mix_Volume(channel, -1);
+}
---
+++ src/MF-FighterTraighter/MF-FighterTraighter/AudioManager.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <iostream>
+#include <assert.h>
+#include <string>
+#include <vector>
+#include <SDL_mixer.h>
+
+class AudioManager
+{
+public:
+	AudioManager();
+	AudioManager(int channels);
+
+	virtual ~AudioManager();
+
+	//SFX
+	Mix_Chunk* loadSFX(const std::string& fileName);
+	virtual void playSFX(Mix_Chunk* sound, int loops, int channel = -1);
+
+	// music
+	Mix_Music* loadMusic(const std::string& fileName);
+	void playMusic(Mix_Music* music, bool loops);
+	virtual int setMusicVolume(int volume);
+	void stopMusic();
+	virtual void pauseMusic();
+	virtual void resumeMusic();
+
+	void resumeAll();
+
+	int getGeneralVolume() const;
+	int getMusicVolume() const;
+	int getChannelVolume(int channel) const;
+
+	//for settings
+	void setGeneralVolume(float MaxVolume, float volume_ratio);
+	void setChannelvolume(int channel, float volume_ratio);
+	void setMusicVolume(float volume_ratio);
+	void setSFXVolume(const std::string& name, float volume_ratio);
+
+private:
+	bool initialized_ = false;
+	int channels_ = -1;
+};
+
