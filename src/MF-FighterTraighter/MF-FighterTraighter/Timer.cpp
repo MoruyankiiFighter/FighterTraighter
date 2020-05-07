551af6e2df78546e8aabfba2ec77ec134b973c69
Anass <anasca01@ucm.es>
2020-05-07 21:06:52 +0200

    Timer idea

---
+++ src/MF-FighterTraighter/MF-FighterTraighter/Timer.cpp
@@ -0,0 +1,22 @@
+#include "Timer.h"
+
+Timer::Timer()
+{
+	if (SDL_Init(SDL_INIT_TIMER)<0)
+	{
+		std::cout << SDL_GetError();
+	}
+	m_LastTime = 0;
+	m_DeltaTime = 0;
+}
+
+ void Timer::Tick()
+{
+	 m_DeltaTime = (SDL_GetTicks() - m_LastTime);/* *FRAME_PER_SECOND;*/
+	 if (m_DeltaTime> MAX_DELTA_TIME)
+	 {
+		 m_DeltaTime = MAX_DELTA_TIME;
+	 }
+	 m_LastTime = SDL_GetTicks();
+	 SDL_Delay(FRAME_DELAY)
+}
--- src/MF-FighterTraighter/MF-FighterTraighter/ecs.h
+++ src/MF-FighterTraighter/MF-FighterTraighter/ecs.h
@@ -36,6 +36,7 @@ namespace ecs {
 		GamepadInput,
 		RenderAnimation,
 		Bullet,
+		Particle,
 		/*
 		
 		*/
