c94dd7810f154ffe746f18e3f62c37fb2995f70f
ancues02 <ancues02@ucm.es>
2020-05-10 15:21:30 +0200

    puesto habilidades de antes en fight

--- src/MF-FighterTraighter/MF-FighterTraighter/Fight.cpp
+++ src/MF-FighterTraighter/MF-FighterTraighter/Fight.cpp
@@ -44,8 +44,8 @@ void Fight::init()
 	//Entity* player1 = FactoryMk::addMockToGame(app_, this, 1, { SDL_SCANCODE_LEFT, SDL_SCANCODE_RIGHT, SDL_SCANCODE_UP, SDL_SCANCODE_DOWN, SDL_SCANCODE_Q, SDL_SCANCODE_E, SDL_SCANCODE_Z, SDL_SCANCODE_X,
 	//	SDL_SCANCODE_1, SDL_SCANCODE_2, SDL_SCANCODE_SPACE, SDL_SCANCODE_R }, world, false, PLAYER_1, PLAYER_2 | WALLS | BOUNDARY | BULLET, 0);
 	Entity* player1 = CharFactory::addCharacterToGame(app_, this, 1, world, &app_->getGameManager()->getPlayerInfo(1), PLAYER_1, PLAYER_2 | WALLS | BOUNDARY | BULLET, 0);
-	player1->getComponent<PlayerAttacks>(ecs::PlayerAttacks)->setAbility(AbilityFactory::GiveAbility(GameManager::AbilityID::ExplosiveWillpower, player1), 0);
-	player1->getComponent<PlayerAttacks>(ecs::PlayerAttacks)->setAbility(AbilityFactory::GiveAbility(GameManager::AbilityID::MegatonGrip, player1), 1);
+	player1->getComponent<PlayerAttacks>(ecs::PlayerAttacks)->setAbility(AbilityFactory::GiveAbility(GameManager::AbilityID::AcidSplit, player1), 0);
+	player1->getComponent<PlayerAttacks>(ecs::PlayerAttacks)->setAbility(AbilityFactory::GiveAbility(GameManager::AbilityID::SeismicShock, player1), 1);
 	vector<std::string>abilitiesP1;
 	abilitiesP1.push_back("MegatonGrip");
 	abilitiesP1.push_back("AcidSplit");
