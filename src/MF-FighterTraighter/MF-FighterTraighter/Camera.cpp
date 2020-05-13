786f07d34b252f4b06e0f79e09545866aa14ac3a
Anass <anasca01@ucm.es>
2020-05-13 17:13:50 +0200

    Camara bonita

--- src/MF-FighterTraighter/MF-FighterTraighter/Camera.cpp
+++ src/MF-FighterTraighter/MF-FighterTraighter/Camera.cpp
@@ -1,29 +1,11 @@
 #include "Camera.h"
 
 
-/*void Camera::update()
-{
-	if (!timerStopped_) {
-		if (!countdown_) {
-			timerNow_ = SDL_GetTicks();
-		}
-		else {
-			timerStart_ = SDL_GetTicks();
-			if (timerNow_ - timerStart_ <= 0) {
-				timerStart_ = timerNow_;
-			}
-		}
-		
-	}
-}*/
-
-
-
 inline void Camera::CalculaPunto()
 {
 	if (m_Target2!=nullptr)
 	{
-		m_ViewBox.y =-(m_Target1->getPosition().getY() + m_Target2->getPosition().getY())/2   +m_shake.y;
+		m_ViewBox.y =-(m_Target1->getPosition().getY() + m_Target2->getPosition().getY())/2   +m_shake.y+ m_Target2->getHeight()/2;
 		m_ViewBox.x = -(m_Target1->getPosition().getX() + m_Target2->getPosition().getX())/2  + m_shake.x;
 	}
 	else if(m_Target1!= nullptr)
