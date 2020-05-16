fcc1526e4a5778dae646ba2d52c5259497a92a3d
Amph <amprubio@ucm.es>
2020-05-16 15:57:40 +0100

    Update CharacterSelectionHandler.cpp

--- src/MF-FighterTraighter/MF-FighterTraighter/CharacterSelectionHandler.cpp
+++ src/MF-FighterTraighter/MF-FighterTraighter/CharacterSelectionHandler.cpp
@@ -7,7 +7,7 @@ void CharacterSelectionHandler::init()
 {
 	ent = new Entity();
 	ent->addComponent<Transform>(Vector2D(0, app_->getWindowManager()->getCurResolution().h/4), Vector2D(), app_->getWindowManager()->getCurResolution().w, app_->getWindowManager()->getCurResolution().h / 4,0);
-	ent->addComponent<RenderImage>(app_->getAssetsManager()->getTexture(AssetsManager::Player));
+	ent->addComponent<RenderImage>(app_->getAssetsManager()->getTexture(AssetsManager::Panel));
 }
 
 void CharacterSelectionHandler::update()
@@ -25,9 +25,7 @@ void CharacterSelectionHandler::render()
 
 void CharacterSelectionHandler::handleInput()
 {
-	if (j1_ && j2_) {
+	if ((j1_ && j2_)&& SDL_GetTicks()%50==0) {
 		app_->getStateMachine()->pushState(new Fight(app_));
 	}
 }
-
-
