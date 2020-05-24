beed5745e6ab0b8e75484f7cc0feaf0d8cee0690
Eloy Moreno <eloymore@ucm.es>
2020-05-24 15:03:57 +0200

    Puesto empate en caso de que el timepo de la ronda se acabe (ningún jugador gana).

--- src/MF-FighterTraighter/MF-FighterTraighter/Fight.cpp
+++ src/MF-FighterTraighter/MF-FighterTraighter/Fight.cpp
@@ -11,6 +11,7 @@
 #include "CharFactory.h"
 #include "AbilitiesTimerFunction.h"
 #include "FightController.h"
+#include "Entity.h"
 
 #include "Camera.h"
 #include "Shake.h"
@@ -154,14 +155,6 @@ void Fight::init()
 
 
 
-	//Timer Ent
-	Entity* timer = entManager_.addEntity();
-	timer->addComponent<UITransform>(Vector2D(0, 75), Vector2D(app_->getWindowManager()->getCurResolution().w / 2, 0), Vector2D(200, 50), Vector2D(400, 100));
-	timer->addComponent<TextComponent>("0000", app_->getAssetsManager()->getFont(AssetsManager::Roboto_Black), 45, TextComponent::Center);
-	timer->addComponent<UITimer>(UITimer::Minutes)->setCountdown(240000); //4minutes timer
-
-
-
 	// Healthbar Player1
 	Entity* healthbarBack1 = entManager_.addEntity();
 	healthbarBack1->addComponent<UITransform>(Vector2D(460, 50), Vector2D(0, 0), Vector2D(365, 20), Vector2D(730, 40));
@@ -223,6 +216,14 @@ void Fight::init()
 	gameController->addComponent<UIRoundRenderer>(rightCounter)->setRoundsWon(app_->getGameManager()->getPlayerRounds(2));
 	gameController->addComponent<FightController>(240, 180);
 	entManager_.setHandler(gameController, ecs::Controller);
+
+
+
+	//Timer Ent
+	Entity* timer = entManager_.addEntity();
+	timer->addComponent<UITransform>(Vector2D(0, 75), Vector2D(app_->getWindowManager()->getCurResolution().w / 2, 0), Vector2D(200, 50), Vector2D(400, 100));
+	timer->addComponent<TextComponent>("0000", app_->getAssetsManager()->getFont(AssetsManager::Roboto_Black), 45, TextComponent::Center);
+	timer->addComponent<UITimer>(UITimer::Minutes, true, 240000, [gameController, timer]() { gameController->getComponent<FightController>(ecs::FightController)->PlayerLost(-1); }); //4 minutes timer
 }
 
 void Fight::handleInput()
