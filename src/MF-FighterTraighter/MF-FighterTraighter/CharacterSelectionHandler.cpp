a4cb908111cb2982ce1058ee404660f5a4269363
ancues02 <ancues02@ucm.es>
2020-05-24 18:55:06 +0200

    ahora characterSelection difiere si entras desde 1 v 1 o Arcade

--- src/MF-FighterTraighter/MF-FighterTraighter/CharacterSelectionHandler.cpp
+++ src/MF-FighterTraighter/MF-FighterTraighter/CharacterSelectionHandler.cpp
@@ -2,7 +2,7 @@
 #include "Entity.h"
 #include "UITransform.h"
 #include "Training.h"
-
+#include "AIGameState.h"
 void CharacterSelectionHandler::init()
 {
 	ent = state_->getEntityManager().addEntity();
@@ -14,13 +14,21 @@ void CharacterSelectionHandler::update()
 {
 	RenderImage* rImage = ent->getComponent<RenderImage>(ecs::RenderImage);
 	j1_ = log1->getComponent<CharacterSelectionLogic>(ecs::CharacterSelectionLogic)->getChosen();
-	j2_ = log2->getComponent<CharacterSelectionLogic>(ecs::CharacterSelectionLogic)->getChosen();
+	if(log2)
+		j2_ = log2->getComponent<CharacterSelectionLogic>(ecs::CharacterSelectionLogic)->getChosen();
 
 	if (j1_ && j2_) {
 		--goTimer;
 		if (!rImage->getRendered()) rImage->setRendered(true);
 		if (goTimer == 0) {
 			app_->getStateMachine()->pushState(new Training(app_));
+		}
+	}
+	else if (j1_ && log2 == nullptr) {
+		--goTimer;
+		if (!rImage->getRendered()) rImage->setRendered(true);
+		if (goTimer == 0) {
+			app_->getStateMachine()->pushState(new AIGameState(app_));
 		}
 	}
 	else {
