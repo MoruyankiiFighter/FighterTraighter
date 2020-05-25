29e5cb8629c1480286b7c87707ec2d767208b3bc
Daniel Cortijo <dcortijo@ucm.es>
2020-05-25 23:39:29 +0200

    Puesto aniamtiondata de master

--- src/MF-FighterTraighter/MF-FighterTraighter/MockingbirdData.cpp
+++ src/MF-FighterTraighter/MF-FighterTraighter/MockingbirdData.cpp
@@ -3,9 +3,9 @@
 MockingbirdData::MockingbirdData(double width, double height, double rotation, double jump_impulse, Vector2D ini_pos, double speed, double ini_health, double attack, double defense, int playerNumber) :
 	PlayerData(width, height, rotation, jump_impulse, ini_pos, speed, ini_health, attack, defense, playerNumber)
 {
-	animLength_ = { {4, true, 12}, {4, true, 15}, {2, true, 3}, {1, true, 15}, {4, false, 2}, {12, false, 10}, {7, false, 10}, {9, false, 8},
-	{15, false, 7}, {7, false, 13}, {9, false, 10}, {10, false, 7}, {5, false, 15}, {2, true, 15}, {2, false, 10}, {3, true, 4}, {2, false, 10},
-	{2, false, 3}, {4, true, 12}, {2, false, 7}, {2, false, 7}, {2, false, 15}, {8, true, 10} };
+	animLength_ = { {4, true, 12}, {6, true, 10}, {2, true, 3}, {2, true, 15}, {2, false, 2}, {4, false, 10}, {4, false, 10}, {6, false, 8},
+	{5, false, 7}, {3, false, 13}, {6, false, 10}, {3, false, 7}, {6, false, 15}, {2, true, 15}, {1, false, 10}, {1, true, 4}, {1, false, 10},
+	{2, false, 3}, {2, true, 12}, {2, false, 7}, {3, false, 15}, {2, false, 15}, {2, false, 10} };
 }
 
 void MockingbirdData::init()
